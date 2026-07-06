2380514a54d0d569bd7fe0aae8a47ee2bc7b8ec7
subhodeep2005s <sarkarsubhodeep417@gmail.com>
2026-07-06 21:27:49 +0530

    chore(cors): allow all origins

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ export function createApp(): Application {
   mentorSubscribers();
   matchSubscribers();
 
-  app.use(cors({ origin: env.CORS_ORIGINS, credentials: true }));
+  app.use(cors({ origin: true, credentials: true }));
   app.use(express.json({ limit: "10mb" }));
   app.use(express.urlencoded({ extended: true, limit: "10mb" }));
   app.use(cookieParser());
